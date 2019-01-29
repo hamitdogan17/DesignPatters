ef79c4a8d75f93b7a5942aecd511f8910fbdf26a
hamitdogan17 <hamitdogan17@gmail.com>
2019-01-29 13:57:35 +0300

    Singleton Design Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        // Sadece insert update delete yaptığı için singleton yapılabilir hale getiricem
+        private static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+        }
+    }
+}
