d17af0e1f89f909f9f2f731455a49d5853dc1aa9
hamitdogan17 <hamitdogan17@gmail.com>
2019-01-30 08:15:05 +0300

    Factory Design Pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Burada iş geliştirip duruma göre edlogger mı başkabir logger mı
+            //app configden okumak Örnek log4Net
+            return new EdLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Burada iş geliştirip duruma göre edlogger mı başkabir logger mı
+            //app configden okumak Örnek log4Net
+            return new Log4NetLogger();
+        }
+    }
+
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger.");
+        }
+    }
+    public class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with log4NetLogger.");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+
+            logger.Log();
+        }
+
+        //Bad code
+        //public void Save()
+        //{
+        //    Console.WriteLine("Saved!");
+        //    ILogger logger = new LoggerFactory().CreateLogger();
+
+        //    logger.Log();
+        //}
+    }
+}
